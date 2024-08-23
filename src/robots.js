d0397c58aeab7a99b23e62812b8add82edcd68d8
RyanLee9219 <the9219@gmail.com>
2024-08-22 20:46:18 -0400

    First Commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots =[
+{
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: "Sincere@april.biz"
+},
+{
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: "Shanna@april.biz"
+},
+{
+    id: 3,
+    name: 'Chloe Hahn',
+    username: 'Sam',
+    email: "Chahn@april.biz"
+},
+{
+    id: 4,
+    name: 'Brad Pitt',
+    username: 'Pitty',
+    email: "Bpit@april.biz"
+},
+{
+    id: 5,
+    name: 'Ray Chalres',
+    username: 'Cole',
+    email: "Rcar12@april.biz"
+},
+{
+    id: 6,
+    name: 'Tim Hortons',
+    username: 'Timmy',
+    email: "THortons1@april.biz"
+},
+{
+    id: 7,
+    name: 'Ryan Lee',
+    username: 'Ryan',
+    email: "RLee9219@april.biz"
+},
+{
+    id: 8,
+    name: 'Star Bucks',
+    username: 'Subuck',
+    email: "SBuks777@april.biz"
+},
+{
+    id: 9,
+    name: 'Ramen Killer',
+    username: 'Rocki',
+    email: "RKelly777@april.biz"
+},
+{
+    id: 10,
+    name: 'Hunter Hacker',
+    username: 'Dobule H',
+    email: "HK1234@april.biz"
+},
+]
